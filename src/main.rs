b6f3fd5b78520c784a65227e7bc4c78361ec3712
electric coffee <slench102@gmail.com>
2016-12-18 00:59:44 +0100

    add split function

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 extern crate cards;
 
+fn split<T>(mut left: Vec<T>) -> (Vec<T>, Vec<T>) {
+    let half_way = left.len() / 2;
+    let right = left.split_off(half_way);
+    (left, right)
+}
+
 fn main() {
     let mut deck = cards::make_standard_deck();
     cards::shuffle(&mut deck);
 
+    let (p1, p2) = split(deck);
 }
